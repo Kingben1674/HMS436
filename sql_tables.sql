fa2aa8d011a524c981aa42df481ad9a61a8fed03
dweber4 <124644383+dweber4@users.noreply.github.com>
2023-04-29 03:01:33 -0400

    Add files via upload

---
+++ sql_tables.sql
@@ -0,0 +1,31 @@
+CREATE TABLE staff(
+    staff_id INT NOT NULL PRIMARY KEY,
+	first_name VARCHAR(25) NOT NULL,
+    last_name VARCHAR(25) NOT NULL,
+	occupation VARCHAR(25) NOT NULL,
+	login_id VARCHAR(25) NOT NULL,
+	password VARCHAR(15) NOT NULL           
+);
+
+CREATE TABLE patients(
+  patient_id INT NOT NULL PRIMARY KEY,
+  patient_fname VARCHAR(25) NOT NULL,
+  patient_lname VARCHAR(25) NOT NULL,
+  dob DATE NOT NULL,
+  address VARCHAR(50),
+  phone_number VARCHAR(20),
+  email VARCHAR(50),
+  social_security VARCHAR(11),
+  ethnicity VARCHAR(30)
+);
+
+CREATE TABLE insurance(
+  insurance_id INT NOT NULL PRIMARY KEY,
+  patient_id INT NOT NULL,
+  insurance_name VARCHAR(30) NOT NULL,
+  policy_num VARCHAR(20) NOT NULL,
+  group_num VARCHAR(20) NOT NULL,
+  FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
+);
+
+
