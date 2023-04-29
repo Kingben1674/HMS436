7a32efd3cd133ada8dca342e66dad3fc8918aa45
dweber4 <124644383+dweber4@users.noreply.github.com>
2023-04-29 02:47:26 -0400

    Delete staff_information_table.sql

--- staff_information_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE staffInformation (
-    staff_id INT NOT NULL PRIMARY KEY,
-	password VARCHAR(15) NOT NULL,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
-    occupation VARCHAR(20) NOT NULL,       
-);
