04476ab46775c9c55c9d3d95763e75c46f30c4ed
Kingben1674 <124644364+Kingben1674@users.noreply.github.com>
2023-05-11 17:06:12 -0400

    Added buttons for searching and creating patients

--- secure_area.php
+++ secure_area.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<script type="text/javascript" src="testingapi.js"></script>
-   	<link rel="stylesheet" type="text/css" href="style.css" />
+    <script type="text/javascript" src="testingapi.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
+
 <body>
 
-<div class=bottomStuff>
-        <?php
-        echo '<a href=logout.php> || Logout  ||</a>';
-        echo '<br><Center>';
-        echo '<H1> Admin Home </H1>';
+    <div class=bottomStuff>
+        <a href=logout.php> || Logout ||</a>
 
-        ?>
+        <H1> Admin Home </H1>
+
         <style>
             div.allWeather {
                 margin: 0 auto;
@@ -36,6 +36,49 @@
 
             div.bottomStuff {
                 clear: both;
+            }
+
+            h1, h3 {
+                text-align: center;
+            }
+
+            .choices {
+                display: flex;
+                justify-content: center;
+
+            }
+
+            .buttonChoice {
+                display: flex;
+                align-items: flex-end;
+                text-align: center;
+                float: left;
+                border: 4px black solid;
+                height: 150px;
+                width: 200px;
+                border-radius: 35px;
+                margin: 5px;
+            }
+
+            .image {
+                width: 100px;
+                height: 50px;
+                margin-bottom: 70px;
+                padding-left: 25%;
+                position: relative;
+            }
+
+            .text {
+                padding-left: 3%;
+                position: absolute;
+            }
+
+            button {
+                text-align: center;
+                width: 175px;
+                height: 200px;
+                text-align: center;
+                margin: 5px;
             }
         </style>
         <div class="allWeather">
@@ -112,21 +155,31 @@
                 <p id="weather12">It is sunny today</p>
             </div>
         </div>
-        <br>
-        <br>
+        </br>
+        </br>
 
-        <div class="bottomStuff">
-            <?php
-            echo '<br><H3>';
-            echo '<br>';
-            echo '<a href=patient.php> [ patient]</a>';
+        <div class="choices">
 
+            <button type="button">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/5/55/Magnifying_glass_icon.svg" width="80"
+                    height="80  " />
+                <h3>
+                    Search Patient
+                </h3>
+            </button>
 
-            echo '<br></Center>';
+            <button type="button">
+                <img src="person.svg" width="80"
+                    height="80  " />
+                <h3>
+                    Create Paitent
+                </h3>
+            </button>
 
-            ?>
         </div>
+    </div>
 
 
 </body>
+
 </html>
